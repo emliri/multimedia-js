f852c0acb17a1aaaccd790983ff3bda6ccf7a287
Stephan Hesse <tchakabam@gmail.com>
2018-02-28 15:56:08 +0100

    isoboxer types

---
+++ src/processors/mp4/isoboxer-types.ts
@@ -0,0 +1,37 @@
+export type ISOBoxPropertyBag = {
+
+} & any;
+
+export type ISOFileCursor = {
+  offset: number
+}
+
+export type ISOBox = {
+
+  size: number,
+  type: string,
+  boxes: ISOBox[],
+
+  _offset: number,
+  _root: ISOBox,
+  _raw: DataView,
+  _parent: ISOBox,
+  _cursor: ISOFileCursor,
+  _parsing: boolean
+
+  write: () => number
+  append: (box: ISOBox, pos: number) => void
+  getLength: () => number
+}
+
+export type ISOFile = {
+  boxes: ISOBox[]
+
+  _raw: DataView
+
+  fetch: (type: string) => ISOBox
+  fetchAll: (type: string) => ISOBox[]
+  parse: () => ISOFile
+  write: () => ArrayBuffer
+  append: (box: ISOBox, pos: number) => void
+}
