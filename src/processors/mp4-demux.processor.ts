99b58edd27246374495eac6720e6ac435f26be52
Stephan Hesse <tchakabam@gmail.com>
2018-06-01 19:07:37 +0200

    mp4-demux: extract actual sample windows

--- src/processors/mp4-demux.processor.ts
+++ src/processors/mp4-demux.processor.ts
@@ -1,19 +1,23 @@
 import {Processor} from '../core/processor';
 import {Packet} from '../core/packet';
-import {InputSocket, SocketDescriptor, SocketType} from '../core/socket';
+import {InputSocket, SocketDescriptor, SocketType, OutputSocket} from '../core/socket';
 
 import {getLogger} from '../logger'
 
 const {log} = getLogger('MP4DemuxProcessor')
 
 import {createMp4Demuxer, Mp4Demuxer, Track, Frame, TracksHash, Atom} from '../ext-mod/inspector.js/src/index';
+
 import { PayloadDescriptor } from '../core/mime-type';
 
 import { Mp4Track } from '../ext-mod/inspector.js/src/demuxer/mp4/mp4-track';
+import { BufferProperties } from '../core/buffer';
 
 export class MP4DemuxProcessor extends Processor {
 
     private _demuxer: Mp4Demuxer;
+
+    private _trackIdToOutputs: { [id: number] : OutputSocket} = {};
 
     constructor() {
         super();
@@ -26,16 +30,28 @@ export class MP4DemuxProcessor extends Processor {
       return new SocketDescriptor()
     }
 
+    private _ensureOutputForTrack(track: Track): OutputSocket {
+
+      const payloadDescriptor = new PayloadDescriptor(track.mimeType);
+      const sd = new SocketDescriptor([
+        payloadDescriptor
+      ]);
+
+      if (!this._trackIdToOutputs[track.id]) {
+        this._trackIdToOutputs[track.id] = this.createOutput(sd);
+      }
+
+      return this._trackIdToOutputs[track.id];
+    }
+
     protected processTransfer_(inS: InputSocket, p: Packet) {
 
       p.data.forEach((bufferSlice) => {
 
         this._demuxer.append(bufferSlice.getUint8Array());
-
         this._demuxer.end();
 
         const tracks: TracksHash = this._demuxer.tracks;
-
         const atoms: Atom[] = this._demuxer.getAtoms();
 
         for (const trackId in tracks) {
@@ -49,15 +65,30 @@ export class MP4DemuxProcessor extends Processor {
 
           log('defaults', track.getDefaults())
 
+          const output: OutputSocket = this._ensureOutputForTrack(track);
+
+          const props: BufferProperties = new BufferProperties(
+            track.mimeType,
+            track.getDefaults().sampleDuration,
+            NaN,
+            1
+          );
+
+          // General time-delta applied to these tracks buffers
+          props.timestampDelta = 0;
+
           track.getFrames().forEach((frame: Frame) => {
 
-            log('frame:', frame.frameType, frame.size, frame.timeUs);
+            //log('frame:', frame.frameType, frame.size, frame.getDecodingTimeUs(), frame.getPresentationTimeUs(), frame.bytesOffset);
 
+            const p: Packet = Packet.fromSlice(bufferSlice.unwrap(frame.bytesOffset, frame.size, props));
+
+            // timestamps of this packet
+            p.timestamp = frame.getDecodingTimestampInSeconds();
+            p.presentationTimeOffset = frame.getPresentationTimestampInSeconds() - frame.getDecodingTimestampInSeconds();
+
+            output.transfer(p)
           })
-
-          this.createOutput(new SocketDescriptor([
-            new PayloadDescriptor(track.mimeType)
-          ]));
         }
 
       })
