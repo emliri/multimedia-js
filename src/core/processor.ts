e36107c5d71857ad107fb9a3d4190280a5fd63fa
Stephan Hesse <tchakabam@gmail.com>
2018-06-16 06:24:51 +0200

    processor: enable handling / proxying symbolic pakcets

--- src/core/processor.ts
+++ src/core/processor.ts
@@ -1,5 +1,5 @@
 import {SocketDescriptor, SocketType, InputSocket, OutputSocket, SocketOwner, Socket} from './socket';
-import {Packet} from './packet';
+import {Packet, PacketSymbol} from './packet';
 import { Signal, SignalReceiver, SignalHandler, SignalReceiverCastResult, collectSignalReceiverCastResults } from './signal';
 
 export abstract class Processor implements SocketOwner, SignalReceiver {
@@ -7,6 +7,8 @@ export abstract class Processor implements SocketOwner, SignalReceiver {
     private inputs_: InputSocket[];
     private outputs_: OutputSocket[];
     private onSignal_: SignalHandler;
+
+    public enableSymbolProxying: boolean = true;
 
     constructor(onSignal?: SignalHandler) {
         this.inputs_ = [];
@@ -22,7 +24,7 @@ export abstract class Processor implements SocketOwner, SignalReceiver {
     }
 
     cast(signal: Signal): SignalReceiverCastResult {
-        return this.onSignal_(signal).then((result) => {
+        return this.onSignalCast_(signal).then((result) => {
             if(result) {
                 return Promise.resolve(true);
             } else {
@@ -91,9 +93,35 @@ export abstract class Processor implements SocketOwner, SignalReceiver {
         return s;
     }
 
-    protected abstract processTransfer_(inS: InputSocket, p: Packet): boolean;
+    /**
+     * @param p
+     * @returns True when packet was forwarded
+     */
+    private onSymbolicPacketReceived_(p: Packet): boolean {
+        const proxy = this.handleSymbolicPacket_(p.symbol);
+        if (proxy && this.enableSymbolProxying) {
+          console.log('proxy symbol', p.symbol)
+          this.transferPacketToAllOutputs_(p);
+          return true;
+        }
+        return false;
+    }
+
+    /**
+     * @param p packet to transfer to all outputs
+     */
+    private transferPacketToAllOutputs_(p: Packet) {
+        this.out.forEach((socket) => {
+          socket.transfer(p);
+        })
+    }
 
     private onReceiveFromInput_(inS: InputSocket, p: Packet): boolean {
+        if(p.isSymbolic()
+            && this.onSymbolicPacketReceived_(p)) {
+          return true; // when packet was forwarded we don't pass it on for processing
+        }
+
         let result = false
         try {
           result = this.processTransfer_(inS, p);
@@ -115,4 +143,36 @@ export abstract class Processor implements SocketOwner, SignalReceiver {
           return Promise.resolve(false);
         }
     }
+
+    /**
+     * At the same time handler for symbols, as well as
+     * arbiter function to determine if this proc proxies or not specific symbols.
+     *
+     * Default proxies all non-VOID symbols.
+     *
+     * Per design this merely passes in the symbol, not the actual packet.
+     *
+     * Symbolic packets are only supposed to be a shell for the symbol itself,
+     * their other properties should be ignored in that case. They also should
+     * not carry any data.
+     *
+     * If one wants to actually get the handle of a symbolic packet,
+     * it is possible by disabling proxying (return false here in an override of this method)
+     * as in this case these packets will be passed into `processTransfer_`.
+     *
+     * @param symbol
+     * @returns True if the symbolic packet should be proxied
+     */
+    protected handleSymbolicPacket_(symbol: PacketSymbol): boolean {
+      return symbol !== PacketSymbol.VOID;
+    }
+
+    /**
+     * Called when a packet is received on an input socket.
+     * Returns true when packet was handled correctly in some way.
+     * @param inS
+     * @param p
+     */
+    protected abstract processTransfer_(inS: InputSocket, p: Packet): boolean;
+
 }
