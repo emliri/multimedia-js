842a70319576157d1ddd88a55ef2bc655ceda4c1
Stephan Hesse <tchakabam@gmail.com>
2018-07-24 18:58:55 +0200

    test-case: fmp4 to mse wip

--- test-cases/web/fmp4-to-media-source.ts
+++ test-cases/web/fmp4-to-media-source.ts
@@ -5,13 +5,40 @@ import { Tubing, TubingState } from '../../src/core/tubing';
 export class Fmp4ToMediaSource extends TestCase {
 
   private _fmp4ToMediaSource: Tubing;
+  private _videoEl: HTMLVideoElement;
+  private _mediaSource: MediaSource;
 
   constructor(domMountPoint: HTMLElement) {
     super(domMountPoint);
   }
 
   setup() {
-    this._fmp4ToMediaSource = new Multimedia.Tubings.HttpToMediaSourceTubing('/test-data/mp4/v-0576p-1400k-libx264.mp4');
+    this._videoEl = document.createElement('video');
+
+    this._videoEl.addEventListener('error', () => {
+      console.error(this._videoEl.error);
+    })
+
+    this.domMountPoint.appendChild(this._videoEl);
+
+    this._mediaSource = new MediaSource();
+
+    console.log('MediaSource created');
+
+    this._videoEl.src = URL.createObjectURL(this._mediaSource);
+
+    this._mediaSource.addEventListener('sourceopen', () => {
+      if (this._fmp4ToMediaSource) {
+        return;
+      }
+
+      console.log('MediaSource opened');
+
+      this._fmp4ToMediaSource
+        = new Multimedia.Tubings.HttpToMediaSourceTubing(
+        '/test-data/mp4/v-0576p-1400k-libx264.mp4', this._mediaSource);
+
+    });
   }
 
   run() {
