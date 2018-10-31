a31588bd146326208e65390fac015d4a03132745
Stephan Hesse <tchakabam@gmail.com>
2018-10-31 14:18:20 +0100

    browser utils

---
+++ src/browser-utils.ts
@@ -0,0 +1,20 @@
+import bowser = require('bowser');
+
+export enum BrowserBrandname {
+  CHROME = 'chrome',
+  FIREFOX = 'firefox',
+  SAFARI = 'safari',
+  IE = 'ie',
+  EDGE = 'edge'
+};
+
+// ADD browser rendering-engines detection; see https://github.com/lancedikson/bowser/tree/v1.x
+
+export function isBrowserBrand(brandName: BrowserBrandname) {
+  return bowser[brandName];
+}
+
+export function isMseSupported(): boolean {
+  return !!(window as any).MediaSource;
+}
+
