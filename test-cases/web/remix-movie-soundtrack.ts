76659becc20e8f879a6dfdaf624272085bb6e15d
Stephan Hesse <tchakabam@gmail.com>
2018-11-19 15:19:42 +0100

    testcase remix movie soundtrack - add filechooser

--- test-cases/web/remix-movie-soundtrack.ts
+++ test-cases/web/remix-movie-soundtrack.ts
@@ -7,25 +7,57 @@ export class RemixMovieSoundtrack extends MmjsTestCase {
 
   setup(done: () => void) {
 
+    const audioFileInput = document.createElement('input');
+    const videoFileInput = document.createElement('input');
+
+    audioFileInput.type = 'file';
+    audioFileInput.id = 'audioFileInput';
+    audioFileInput.accept = 'audio/mp4';
+
+    videoFileInput.type = 'file';
+    videoFileInput.id = 'videoFileInput';
+    videoFileInput.accept = 'video/mp4,video/quicktime';
+
+    this.domMountPoint.appendChild(document.createElement('br'))
+    this.domMountPoint.appendChild(document.createTextNode('Audio file:'))
+    this.domMountPoint.appendChild(audioFileInput);
+    this.domMountPoint.appendChild(document.createElement('br'))
+    this.domMountPoint.appendChild(document.createTextNode('Video file:'))
+    this.domMountPoint.appendChild(videoFileInput);
+
+    this.domMountPoint.appendChild(document.createElement('br'))
+    const processButton = document.createElement('button');
+    processButton.innerText = 'Process'
+
+    this.domMountPoint.appendChild(processButton);
+    this.domMountPoint.appendChild(document.createElement('br'))
+
     const videoEl = document.createElement('video');
     videoEl.controls = true;
     videoEl.addEventListener('error', () => {
       console.error(videoEl.error);
     });
+    this.domMountPoint.appendChild(videoEl);
 
     const mediaSource = new MediaSource();
 
     videoEl.src = URL.createObjectURL(mediaSource);
 
-    this._flow = new MovToFmp4Flow(
-      '/test-data/mp4/v-0576p-1400k-libx264.mov',
-      //'/test-data/mp3/shalafon.mp3',
-      //'/test-data/guitars.m4a',
-      '/test-data/mp4/KickOutTheJams.mp4',
-      //'/test-data/mp3/212438__pcfstnk__ubahn.mp3',
-      mediaSource);
+    processButton.onclick = () => {
 
-    this.domMountPoint.appendChild(videoEl);
+      const videoUrl = URL.createObjectURL(videoFileInput.files[0])
+      const audioUrl = URL.createObjectURL(audioFileInput.files[0]);
+
+      //const videoUrl = '/test-data/mp4/v-0576p-1400k-libx264.mov';
+      //const audioUrl = '/test-data/mp4/KickOutTheJams.mp4';
+
+      this._flow = new MovToFmp4Flow(
+        videoUrl,
+        audioUrl,
+        mediaSource);
+    }
+
+
   }
 
   run() {}
