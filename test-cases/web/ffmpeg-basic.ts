eae0f667fe69d18ea0aabfb23d849cb29d86dad1
Stephan Hesse <tchakabam@gmail.com>
2018-11-30 20:39:41 +0100

    ffmpeg basic testcase: try to convert mp3 file to mp4

--- test-cases/web/ffmpeg-basic.ts
+++ test-cases/web/ffmpeg-basic.ts
@@ -2,6 +2,7 @@ import { MmjsTestCase } from "../mmjs-test-case";
 
 import { FFmpegConverter } from '../../src/processors/ffmpeg/ffmpeg';
 import { getLogger } from "../../src/logger";
+import { makeGetRequest } from "../../src/common-http";
 
 const {log, error} = getLogger('ffmpeg-basic-testcase');
 
@@ -13,6 +14,14 @@ export class FFmpegBasic extends MmjsTestCase {
     const ffmpegWrapper = new FFmpegConverter(ffmpeg);
 
     ffmpegWrapper.getVersion().then((version) => log('ffmpeg version:', version));
+
+    makeGetRequest('/test-data/mp3/212438__pcfstnk__ubahn.mp3').then((data) => {
+
+      log('got input data:', data);
+
+
+
+    });
   }
 
   run() {}
