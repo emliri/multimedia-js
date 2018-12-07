67c8c531abf01e36eab1ab421302804c4c2297f8
Stephan Hesse <tchakabam@gmail.com>
2018-12-07 19:26:46 +0100

    web file download: use file-saver

--- src/io-sockets/web-file-download.socket.ts
+++ src/io-sockets/web-file-download.socket.ts
@@ -5,6 +5,8 @@ import { concatArrayBuffers, makeTemplate } from "../common-utils";
 import { UNKNOWN_MIMETYPE } from "../core/payload-description";
 import { getLogger } from "../logger";
 
+import * as FileSaver from 'file-saver';
+
 export const DEFAULT_FILENAME_TEMPLATE = makeTemplate("buffer${counter}-${Date.now()}.data");
 export const DEFAULT_HTML_TEMPLATE = makeTemplate("<p>Download ${fileName}</p>");
 
@@ -17,12 +19,12 @@ export class WebFileDownloadSocket extends InputSocket {
 
   /**
    *
-   * @param _downloadLinkContainer
+   * @param _downloadLinkContainer an HTML element to create a link in with the given templates or `null` and then FileSaver will spawn a dialog @see https://github.com/eligrey/FileSaver.js
    * @param _fileNameTemplate can contain the local var `counter` as well as access public and private members via `this`, see default
    * @param _htmlTemplate can contain the local var `fileName` which will be whatever the respective template string has eval'd to, see default
    * @param _mimeType
    */
-  constructor (private _downloadLinkContainer: HTMLElement,
+  constructor (private _downloadLinkContainer: HTMLElement | null,
     private _mimeType: string = UNKNOWN_MIMETYPE, // FIXME: this can be a default when packet has no mime-type but we should read the mime-type from the defaultPayload info of the packet
     private _fileNameTemplate: string = DEFAULT_FILENAME_TEMPLATE,
     private _htmlTemplate = DEFAULT_HTML_TEMPLATE) {
@@ -43,19 +45,23 @@ export class WebFileDownloadSocket extends InputSocket {
 
       const blob = new Blob([this._accuBuffer], { type: this._mimeType });
       const objectUrl = URL.createObjectURL(blob);
-      const link = document.createElement('a');
 
       // local template eval vars
-      let counter = this._bufferDownloadCnt;
-      const fileName = eval(this._fileNameTemplate);
+      const counter = this._bufferDownloadCnt++;
+      const filename = eval(this._fileNameTemplate);
 
-      link.href = objectUrl;
-      link.download = fileName;
-      link.innerHTML = eval(this._htmlTemplate);
-      this._downloadLinkContainer.appendChild(link);
+      if (!this._downloadLinkContainer) {
 
-      this._bufferDownloadCnt = ++counter;
+        FileSaver.saveAs(blob, filename);
 
+      } else {
+
+        const link = document.createElement('a');
+        link.href = objectUrl;
+        link.download = filename;
+        link.innerHTML = eval(this._htmlTemplate);
+        this._downloadLinkContainer.appendChild(link);
+      }
     });
 
     return true;
