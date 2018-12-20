3c8f8c99299b123acef34541940fba9795bea3bd
Stephan Hesse <tchakabam@gmail.com>
2018-12-20 12:56:58 +0100

    core env: export setter/getter methods for env vars

--- src/core/env.ts
+++ src/core/env.ts
@@ -1,7 +1,24 @@
-const PROXY_WORKER_PATH = '/dist/MMProcessorProxyWorker.umd.js'; // FIXME nasty HACK!! -> make this configurable
+/**
+ * Defaults
+ */
+const PROXY_WORKER_PATH = '/dist/MMProcessorProxyWorker.umd.js';
 const FFMPEG_BIN_PATH = '/vendor/ffmpeg.js/ffmpeg-mp4.js'
 
-export const EnvironmentVars = {
+type EnvironmentVars = {
+  PROXY_WORKER_PATH: string
+  FFMPEG_BIN_PATH: string
+}
+
+export const EnvironmentVars: EnvironmentVars = {
   PROXY_WORKER_PATH,
   FFMPEG_BIN_PATH
 }
+
+export function setEnvironmentVar(name: keyof EnvironmentVars, value: string) {
+  EnvironmentVars[name] = value;
+}
+
+export function getEnvironmentVar(name: keyof EnvironmentVars): string {
+  return EnvironmentVars[name];
+}
+
