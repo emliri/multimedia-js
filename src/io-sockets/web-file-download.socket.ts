db586f851e8d371575fdda4a01a92ec8b1125ed3
Stephan Hesse <tchakabam@gmail.com>
2019-02-13 01:14:01 +0100

    web file download socket: make usage of save-as optional as fallback when no link container set

--- src/io-sockets/web-file-download.socket.ts
+++ src/io-sockets/web-file-download.socket.ts
@@ -1,4 +1,4 @@
-import { InputSocket, SocketDescriptor } from "../core/socket";
+import { InputSocket, SocketDescriptor, SocketEvent } from "../core/socket";
 
 import { Packet } from "../core/packet";
 import { concatArrayBuffers, makeTemplate } from "../common-utils";
@@ -23,14 +23,21 @@ export class WebFileDownloadSocket extends InputSocket {
    * @param _fileNameTemplate can contain the local var `counter` as well as access public and private members via `this`, see default
    * @param _htmlTemplate can contain the local var `filename` which will be whatever the respective template string has eval'd to, see default
    * @param _mimeType mime-type string for the blob object to be created (this sometimes matters for platforms to understand what file-type it is), @see https://developer.mozilla.org/en-US/docs/Web/API/Blob
+   * @param _fallbackToSaveAs when no downloadLinkContainer set, we can call the `saveAs` function to trigger a download dialog (if running in a web-like window context)
    */
   constructor (private _downloadLinkContainer: HTMLElement | null,
     private _mimeType: string = UNKNOWN_MIMETYPE, // FIXME: this can be a default when packet has no mime-type but we should read the mime-type from the defaultPayload info of the packet
     private _fileNameTemplate: string = DEFAULT_FILENAME_TEMPLATE,
-    private _htmlTemplate = DEFAULT_HTML_TEMPLATE) {
+    private _htmlTemplate = DEFAULT_HTML_TEMPLATE,
+    private _fallbackToSaveAs: boolean = true) {
 
     super((p: Packet) => this._onPacketReceived(p), new SocketDescriptor());
 
+    ///*
+    this.on(SocketEvent.DATA_PACKET_RECEIVED, () => {
+      console.warn('data packet received event', this)
+    })
+    //*/
   }
 
   private _onPacketReceived(p: Packet): boolean {
@@ -50,7 +57,7 @@ export class WebFileDownloadSocket extends InputSocket {
       const counter = this._bufferDownloadCnt++;
       const filename = eval(this._fileNameTemplate);
 
-      if (!this._downloadLinkContainer) {
+      if (!this._downloadLinkContainer && this._fallbackToSaveAs) {
 
         log('invoking save-as function with blob/filename', blob, filename)
 
@@ -63,6 +70,7 @@ export class WebFileDownloadSocket extends InputSocket {
         link.download = filename;
         link.innerHTML = eval(this._htmlTemplate);
         this._downloadLinkContainer.appendChild(link);
+
       }
     });
 
