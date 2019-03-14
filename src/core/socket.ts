e4bcc6854b068b7590719477b13acd7339eb59e6
Stephan Hesse <tchakabam@gmail.com>
2019-03-14 22:23:34 +0100

    socket: allow to serialize socket descriptors to json and back from it

--- src/core/socket.ts
+++ src/core/socket.ts
@@ -42,6 +42,27 @@ export class SocketDescriptor {
     return new SocketDescriptor(mimeTypes.map((mimeType) => new PayloadDescriptor(mimeType)));
   }
 
+  static fromPayloads (payloads: PayloadDescriptor[]): SocketDescriptor {
+    return new SocketDescriptor(payloads);
+  }
+
+  static fromJson(serializedSd: string): SocketDescriptor {
+    const sd: SocketDescriptor = JSON.parse(serializedSd);
+    // now lets brings this dead thing back to life
+    return SocketDescriptor.fromPayloads(
+      sd.payloads.map((payload) => {
+        const pd = new PayloadDescriptor(
+          payload.mimeType,
+          payload.sampleRateInteger,
+          payload.sampleDepth,
+          payload.sampleDurationNumerator
+        );
+        pd.details = payload.details;
+        return pd;
+      })
+    )
+  }
+
   // TODO: also allow to directly bind this to proc templateSocketDescriptor method on construction
   static createTemplateGenerator (
     inputSd: SocketDescriptor, outputSd: SocketDescriptor): SocketTemplateGenerator {
@@ -55,12 +76,30 @@ export class SocketDescriptor {
 
   readonly payloads: PayloadDescriptor[];
 
+  payload(): PayloadDescriptor {
+    if (this.payloads.length > 1) {
+      throw new Error('Socket descriptor has more than on payload descriptor');
+    }
+    if (this.payloads.length === 0) {
+      throw new Error('Socket descriptor has no payload descriptors');
+    }
+    return this.payloads[0];
+  }
+
   constructor (payloads?: PayloadDescriptor[]) {
     this.payloads = payloads || [];
   }
 
   isVoid (): boolean {
     return this.payloads.length === 0;
+  }
+
+  toJson(): string {
+    try {
+      return JSON.stringify(this);
+    } catch (err) {
+      throw new Error('Could not serialize socket descriptor. JSON error: ' + err.messsage);
+    }
   }
 }
 
