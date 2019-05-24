e46dca58a7f6d70e4860efae0e1856ca88132155
Stephan Hesse <tchakabam@gmail.com>
2019-05-24 15:47:20 +0200

    core: fifo: implement concept of "valve" in order to store packets from an input and drain them to an output. allow to wrap an existing output with a valve. this should allow to collect all packets coming from an output and let them out in a controlled way, or peeking into the buffer data to adapt the flow setup respectively.

--- src/core/fifo.ts
+++ src/core/fifo.ts
@@ -1,8 +1,8 @@
 import { Packet } from './packet';
-import { InputSocket, SocketDescriptor } from './socket';
+import { InputSocket, SocketDescriptor, OutputSocket } from './socket';
 import { VoidCallback } from '../common-types';
 
-export class Fifo extends InputSocket {
+export class FifoQueue extends InputSocket {
   private _packets: Packet[] = [];
 
   constructor (
@@ -22,7 +22,22 @@ export class Fifo extends InputSocket {
     if (this._packets.length === 0) {
       return null;
     }
+    return this._packets.shift();
+  }
+
+  dequeue(): Packet {
+    if (this._packets.length === 0) {
+      return null;
+    }
     return this._packets.pop();
+  }
+
+  drop() {
+    this._packets = [];
+  }
+
+  get length(): number {
+    return this._packets.length;
   }
 
   private _onReceive (p: Packet): boolean {
@@ -35,3 +50,37 @@ export class Fifo extends InputSocket {
     this._onPacketWasQueued();
   }
 }
+
+export class FifoValve extends OutputSocket {
+
+  constructor(
+    private _queue: FifoQueue,
+    sd: SocketDescriptor
+  ) {
+    super(sd);
+  }
+
+  get queue() { return this._queue; }
+
+  passOne() {
+    this.transfer(this._queue.pop());
+  }
+
+  drain() {
+    while(this._queue.length) {
+      this.passOne();
+    }
+  }
+}
+
+export function wrapOutputSocketWithValve(
+  output: OutputSocket,
+  onPacketWasQueued: VoidCallback = () => {}): FifoValve {
+
+  const q = new FifoQueue(onPacketWasQueued)
+  output.connect(q)
+
+  return new FifoValve(q, output.descriptor())
+}
+
+
