11328b1b55f13e8079dfbdfd04aa4776265da874
Stephan Hesse <tchakabam@gmail.com>
2019-07-16 14:40:41 +0200

    inspect mp4 test case: support for audio (AAC parse proc)

---
+++ src/processors/aac-parse.processor.ts
@@ -0,0 +1,82 @@
+import { Processor, ProcessorEvent } from '../core/processor';
+import { Packet } from '../core/packet';
+import { InputSocket, SocketDescriptor, SocketType } from '../core/socket';
+
+import { BufferSlice } from '../core/buffer';
+
+import { getLogger, LoggerLevel } from '../logger';
+import { Esds } from '../ext-mod/inspector.js/src/demuxer/mp4/atoms/esds';
+import { parseAacADTSHeaderInfo, makeAACAudioSpecificConfigMp4DataFromADTSHeader, isAacADTSHeaderPattern } from './aac/adts-utils';
+
+const { log, warn, error } = getLogger('AACParseProcessor', LoggerLevel.ON, true);
+
+export class AACParseProcessor extends Processor {
+  static getName (): string {
+    return 'AACParseProcessor';
+  }
+
+  constructor () {
+    super();
+
+    this.createInput();
+    this.createOutput();
+  }
+
+  templateSocketDescriptor (st: SocketType): SocketDescriptor {
+    return new SocketDescriptor();
+  }
+
+  protected processTransfer_ (inS: InputSocket, p: Packet) {
+
+    log('parsing packet:', p.toString());
+
+    p.forEachBufferSlice(
+      this._onBufferSlice.bind(this, p),
+      this._onProcessingError.bind(this),
+      this);
+
+    this.out[0].transfer(
+      p
+    );
+
+    return true;
+  }
+
+  private _onProcessingError (bufferSlice: BufferSlice, err: Error): boolean {
+    error('error:', err);
+
+    return false;
+  }
+
+  private _onBufferSlice (p: Packet, bufferSlice: BufferSlice) {
+
+    if (p.defaultPayloadInfo) {
+      if (p.defaultPayloadInfo.isBitstreamHeader) {
+        log('packet has bitstream header flag')
+
+        const esds: Esds = <Esds> Esds.parse(bufferSlice.getUint8Array())
+
+        log('parsed MP4 audio-atom:', esds);
+      }
+      if (p.defaultPayloadInfo.isKeyframe) {
+        log('packet has keyframe flag')
+      }
+    } else {
+      warn('no default payload info')
+    }
+
+    const data = bufferSlice.getUint8Array();
+
+    if (!isAacADTSHeaderPattern(data, 0)) {
+      log('No ADTS header-pattern found')
+      return;
+    }
+
+    log('AAC audio config:', makeAACAudioSpecificConfigMp4DataFromADTSHeader(data, 0))
+
+    log('ADTS header-info:', parseAacADTSHeaderInfo(data, 0, 0, 0, 0))
+
+
+  }
+
+}
--- test-cases/web/inspect-mp4.ts
+++ test-cases/web/inspect-mp4.ts
@@ -5,6 +5,11 @@ import { XhrSocket } from "../../src/io-sockets/xhr.socket";
 import { ProcessorEvent, ProcessorEventData } from "../../src/core/processor";
 import { H264ParseProcessor } from "../../src/processors/h264-parse.processor";
 import { OutputSocket } from "../../src/core/socket";
+import { AACParseProcessor } from "../../src/processors/aac-parse.processor";
+
+
+const ENABLE_AUDIO = true;
+const ENABLE_VIDEO = false;
 
 export class InspectMp4 extends MmjsTestCase {
 
@@ -17,21 +22,29 @@ export class InspectMp4 extends MmjsTestCase {
   run() {
 
     let h264Parse;
+    let aacParse;
 
     const mp4Demux = newProcessorWorkerShell(MP4DemuxProcessor);
     mp4Demux.on(ProcessorEvent.OUTPUT_SOCKET_CREATED, (data: ProcessorEventData) => {
 
-      if (data.socket.payload().isVideo()) {
+      if (ENABLE_VIDEO && data.socket.payload().isVideo()) {
         h264Parse = newProcessorWorkerShell(H264ParseProcessor);
         OutputSocket.fromUnsafe(data.socket).connect(h264Parse.in[0])
+      }
+
+      else if (ENABLE_AUDIO && data.socket.payload().isAudio()) {
+        aacParse = newProcessorWorkerShell(AACParseProcessor);
+        OutputSocket.fromUnsafe(data.socket).connect(aacParse.in[0])
       }
 
     });
 
     const xhrSocket: XhrSocket = new XhrSocket(
-      //'/test-data/mp4/SampleVideo_720x480_10mb.mp4',
+      '/test-data/mp4/SampleVideo_720x480_10mb.mp4',
       //'/test-data/mp4/SampleVideo_1280x720_5mb.mp4'
-      '/test-data/mp4/buffer0-1562764964156.mp4',
+      //'/test-data/mp4/buffer0-1562764964156.mp4',
+      //'/test-data/mp4/180312_unicorn_hütte2_s.mp4',
+      //'/test-data/mp4/01_Closing_campaign_4k.mp4',
       //'/test-data/mp4/ffmpeg-concat-out.mp4',
       //'/test-data/mp4/v-0576p-1400k-libx264.mp4'
       //'/test-data/mp4/180312_unicorn_hütte2_s.mp4'
