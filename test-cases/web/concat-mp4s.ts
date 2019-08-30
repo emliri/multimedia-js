00592648571951aa6699d3eb9cec7d44a0c953f3
Stephan Hesse <tchakabam@gmail.com>
2019-08-30 02:33:24 +0200

    concat mp4s: update test file paths

--- test-cases/web/concat-mp4s.ts
+++ test-cases/web/concat-mp4s.ts
@@ -9,12 +9,12 @@ export class ConcatMp4s extends MmjsTestCase {
   setup(done: () => void) {
 
     this._flow = new ConcatMp4sFlow(
-      '/test-data/mp4/v-0576p-1400k-libx264.mp4', // FIXME
-      '/test-data/mp4/01_Closing_campaign_4k.mp4', // FIXME
-      //'/test-data/mp4/SampleVideo_720x480_10mb.mp4',
-      //'/test-data/mp4/SampleVideo_1280x720_5mb.mp4',
-      //'/test-data/mp4/180312_unicorn_hütte2_s.mp4',
+
+      //'/test-data/mp4/v-0576p-1400k-libx264.mp4',
       //'/test-data/mp4/01_Closing_campaign_4k.mp4',
+      '/test-data/mp4/SampleVideo_720x480_10mb.mp4',
+      '/test-data/mp4/SampleVideo_1280x720_5mb.mp4',
+      //'/test-data/mp4/180312_unicorn_hütte2_s.mp4',
       //'/test-data/video-2018-10-04T18_54_27.577Z.mp4',
       //'/test-data/mp4/v-0576p-1400k-libx264.mp4',
 
