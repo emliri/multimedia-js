7474bf1c7bec670fe8c8e7f3b980583b7a7f69d4
Stephan Hesse <tchakabam@gmail.com>
2020-03-26 01:27:48 +0100

    http-2-mse flow: rm actual mse instance, use app socket flag

--- src/flows/http-to-media-source.flow.ts
+++ src/flows/http-to-media-source.flow.ts
@@ -1,9 +1,9 @@
 import { XhrSocket } from '../io-sockets/xhr.socket';
 import { MP4DemuxProcessor } from '../processors/mp4-demux.processor';
 import { MPEGTSDemuxProcessor } from '../processors/mpeg-ts-demux.processor';
-import { MP4MuxProcessor, MP4MuxProcessorSupportedCodecs } from '../processors/mp4-mux-mozilla.processor';
-import { Flow, FlowStateChangeCallback, FlowConfigFlag } from '../core/flow';
-import { Socket, OutputSocket } from '../core/socket';
+import { MP4MuxProcessor } from '../processors/mp4-mux-mozilla.processor';
+import { Flow, FlowConfigFlag } from '../core/flow';
+import { OutputSocket } from '../core/socket';
 import { H264ParseProcessor } from '../processors/h264-parse.processor';
 import { HTML5MediaSourceBufferSocket } from '../io-sockets/html5-media-source-buffer.socket';
 import { ProcessorEvent, ProcessorEventData } from '../core/processor';
@@ -15,17 +15,17 @@ import { newProcessorWorkerShell } from '../core/processor-factory';
 const { log } = getLogger('HttpToMediaSourceFlow', LoggerLevel.ON, true);
 
 const ENABLE_AUDIO = false
-
 const ENABLE_VIDEO = true
 export class HttpToMediaSourceFlow extends Flow {
+
   private _xhrSocket: XhrSocket;
 
   private _haveVideo = false;
   private _haveAudio = false;
 
-  constructor (private _url: string, private _mediaSource: MediaSource) {
+  constructor (private _url: string) {
     super(
-      FlowConfigFlag.NONE | FlowConfigFlag.WITH_DOWNLOAD_SOCKET,
+      FlowConfigFlag.NONE | FlowConfigFlag.WITH_DOWNLOAD_SOCKET | FlowConfigFlag.WITH_APP_SOCKET,
       (prevState, newState) => {
         log('previous state:', prevState, 'new state:', newState);
       },
@@ -36,18 +36,6 @@ export class HttpToMediaSourceFlow extends Flow {
     );
   }
 
-  protected onCompleted_ (done: VoidCallback) {
-    done();
-  }
-
-  protected onVoidToWaiting_ (done: VoidCallback) {
-    done();
-  }
-
-  protected onWaitingToVoid_ (done: VoidCallback) {
-    done();
-  }
-
   protected onWaitingToFlowing_ (done: VoidCallback) {
     const mp4DemuxProc = newProcessorWorkerShell(MP4DemuxProcessor);
     const tsDemuxProc = newProcessorWorkerShell(MPEGTSDemuxProcessor);
@@ -55,9 +43,6 @@ export class HttpToMediaSourceFlow extends Flow {
     const mp4MuxProc = newProcessorWorkerShell(MP4MuxProcessor);
 
     const xhrSocket = this._xhrSocket = new XhrSocket(this._url);
-
-    const mediaSourceSocket: HTML5MediaSourceBufferSocket =
-      new HTML5MediaSourceBufferSocket(this._mediaSource); // avc1.4d401f
 
     const onDemuxOutputCreated = (data: ProcessorEventData) => {
       const demuxOutputSocket = <OutputSocket> data.socket;
@@ -107,20 +92,30 @@ export class HttpToMediaSourceFlow extends Flow {
     tsDemuxProc.on(ProcessorEvent.OUTPUT_SOCKET_CREATED, onDemuxOutputCreated);
     mp4DemuxProc.on(ProcessorEvent.OUTPUT_SOCKET_CREATED, onDemuxOutputCreated);
 
-    //mp4MuxProc.out[0].connect(mediaSourceSocket);
-
-    this.connectWithAllExternalSockets(mp4MuxProc.out[0])
-
     if (this._url.endsWith('.ts')) { // FIXME use mime-type of response
       xhrSocket.connect(tsDemuxProc.in[0]);
     } else { // FIXME use mime-type of response
       xhrSocket.connect(mp4DemuxProc.in[0]);
     }
 
+    this.connectWithAllExternalSockets(mp4MuxProc.out[0])
+
     done();
   }
 
   protected onFlowingToWaiting_ (done: VoidCallback) {
+    done();
+  }
+
+  protected onCompleted_ (done: VoidCallback) {
+    done();
+  }
+
+  protected onVoidToWaiting_ (done: VoidCallback) {
+    done();
+  }
+
+  protected onWaitingToVoid_ (done: VoidCallback) {
     done();
   }
 
