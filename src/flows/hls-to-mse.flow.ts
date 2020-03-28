978c8243e06d7c81c05371f3c63bd6eb6c3693dd
Stephan Hesse <tchakabam@gmail.com>
2020-03-28 19:56:08 +0100

    hls flow basics

--- src/flows/hls-to-mse.flow.ts
+++ src/flows/hls-to-mse.flow.ts
@@ -11,7 +11,7 @@ import { newProcessorWorkerShell } from '../core/processor-factory';
 import { ProcessorEvent, ProcessorEventData } from '../core/processor';
 import { Flow, FlowConfigFlag } from '../core/flow';
 import { PayloadCodec } from '../core/payload-description';
-import { OutputSocket } from '../core/socket';
+import { OutputSocket, SocketEvent } from '../core/socket';
 
 import { getLogger, LoggerLevel } from '../logger';
 
@@ -21,6 +21,8 @@ const { log } = getLogger('HlsToMediaSourceFlow', LoggerLevel.ON, true);
 
 const ENABLE_AUDIO = false
 const ENABLE_VIDEO = true
+
+const USE_TS_DEMUX = true;
 export class HlsToMediaSourceFlow extends Flow {
 
   private _hlsOutSocket: HlsOutputSocket;
@@ -29,15 +31,16 @@ export class HlsToMediaSourceFlow extends Flow {
   private _haveVideo = false;
   private _haveAudio = false;
 
-  constructor (private _m3u8Url: string) {
+  constructor (private _m3u8Url: string, private _videoEl: HTMLMediaElement) {
     super(
-      FlowConfigFlag.NONE,
+      FlowConfigFlag.NONE, // | FlowConfigFlag.WITH_DOWNLOAD_SOCKET,
       (prevState, newState) => {
         log('previous state:', prevState, 'new state:', newState);
       },
       (reason) => {
         log('state change aborted. reason:', reason);
-      }
+      },
+      //{ el: null, mimeType: 'video/mp4', filenameTemplateBase: 'dump-${new Date().toJSON()}.mp4' }
     );
   }
 
@@ -45,6 +48,9 @@ export class HlsToMediaSourceFlow extends Flow {
 
     this._hlsOutSocket.load(this._m3u8Url);
 
+    this._hlsOutSocket.on(SocketEvent.READY, () => {
+      this._hlsOutSocket.seek(0, 10);
+    });
     done();
   }
 
@@ -57,23 +63,26 @@ export class HlsToMediaSourceFlow extends Flow {
 
     const outSocket = this._hlsOutSocket = new HlsOutputSocket();
 
-    //const mediaSource = new MediaSource();
-    //const inSocket = this._mseInSocket = new HTML5MediaSourceBufferSocket(mediaSource);
+    const mediaSource = new MediaSource();
+    const inSocket = this._mseInSocket = new HTML5MediaSourceBufferSocket(mediaSource);
+
+    this._videoEl.src = URL.createObjectURL(mediaSource);
 
     const onDemuxOutputCreated = (data: ProcessorEventData) => {
       const demuxOutputSocket = <OutputSocket> data.socket;
 
       log('demuxer output created');
 
-      let muxerInputSocket;
+      let mp4MuxerInputSocket;
 
       const payloadDescriptor = demuxOutputSocket.payload();
 
       if (data.processor === mp4DemuxProc) {
 
         demuxOutputSocket.connect(h264ParseProc.in[0]);
-        muxerInputSocket = mp4MuxProc.createInput();
-        h264ParseProc.out[0].connect(muxerInputSocket);
+
+        mp4MuxerInputSocket = mp4MuxProc.createInput();
+        h264ParseProc.out[0].connect(mp4MuxerInputSocket);
 
       } else if (data.processor === tsDemuxProc) {
 
@@ -85,8 +94,9 @@ export class HlsToMediaSourceFlow extends Flow {
           this._haveVideo = true;
 
           demuxOutputSocket.connect(h264ParseProc.in[0]);
-          muxerInputSocket = mp4MuxProc.createInput();
-          h264ParseProc.out[0].connect(muxerInputSocket);
+
+          mp4MuxerInputSocket = mp4MuxProc.createInput();
+          h264ParseProc.out[0].connect(mp4MuxerInputSocket);
 
         } else if (!this._haveAudio &&
             PayloadCodec.isAac(payloadDescriptor.codec)) {
@@ -108,7 +118,7 @@ export class HlsToMediaSourceFlow extends Flow {
     tsDemuxProc.on(ProcessorEvent.OUTPUT_SOCKET_CREATED, onDemuxOutputCreated);
     mp4DemuxProc.on(ProcessorEvent.OUTPUT_SOCKET_CREATED, onDemuxOutputCreated);
 
-    if (this._m3u8Url.endsWith('.ts')) { // FIXME use mime-type of response
+    if (USE_TS_DEMUX) { // FIXME use mime-type of response
       outSocket.connect(tsDemuxProc.in[0]);
     } else { // FIXME use mime-type of response
       outSocket.connect(mp4DemuxProc.in[0]);
--- src/io-sockets/hls/hls-output-socket.ts
+++ src/io-sockets/hls/hls-output-socket.ts
@@ -1,7 +1,12 @@
-import { SeekableOutputSocket, SocketDescriptor, OutputSocket, URLLoadingOutputSocket } from "../../core/socket";
+import { SeekableOutputSocket, SocketDescriptor, OutputSocket, URLLoadingOutputSocket, SocketEvent } from "../../core/socket";
 
 import { HlsLoader } from "../../../../:rialto/lib/hls-loader";
 import { MediaSegment } from "../../../../:rialto/lib/media-segment";
+import { getLogger, LoggerLevel } from "../../logger";
+import { TimeInterval } from "../../../../:rialto/lib/time-intervals";
+import { Packet, PacketSymbol } from "../../core/packet";
+
+const { log } = getLogger('HlsToMediaSourceFlow', LoggerLevel.ON, true);
 
 export class HlsOutputSocket extends OutputSocket implements SeekableOutputSocket, URLLoadingOutputSocket {
 
@@ -12,14 +17,23 @@ export class HlsOutputSocket extends OutputSocket implements SeekableOutputSocke
   }
 
   load(url: string): boolean {
-    this._hlsLoader = new HlsLoader(url, (segment: MediaSegment) => {
-
-    });
+    this._hlsLoader = new HlsLoader(url, this._onMediaUpdate.bind(this), this._onMediaSegmentLoaded.bind(this));
     return true;
   }
 
   seek(start: number, end?: number): boolean {
-    throw new Error("Method not implemented.");
+    this._hlsLoader.getVariantStreams()[0].setFetchTargetRange(new TimeInterval(start, end));
+    return true;
+  }
+
+  private _onMediaUpdate() {
+    log('got variants:', this._hlsLoader.getVariantStreams());
+    this._emit(SocketEvent.READY);
+  }
+
+  private _onMediaSegmentLoaded(segment: MediaSegment) {
+    this.transfer(Packet.fromArrayBuffer(segment.buffer, segment.mimeType));
+    this.transfer(Packet.fromSymbol(PacketSymbol.EOS));
   }
 
 }
