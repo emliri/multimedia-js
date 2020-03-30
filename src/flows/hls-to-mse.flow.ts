2e221c1cfa9ba57fe4ce7c095d5870ac6d843133
Stephan Hesse <tchakabam@gmail.com>
2020-03-30 04:36:11 +0200

    hls2mse flow: cleanup demux-socket-create callback and de-closure internals + play video when setting source

--- src/flows/hls-to-mse.flow.ts
+++ src/flows/hls-to-mse.flow.ts
@@ -16,6 +16,7 @@ import { OutputSocket, SocketEvent } from '../core/socket';
 import { getLogger, LoggerLevel } from '../logger';
 
 import { VoidCallback } from '../common-types';
+import { ProcessorProxy } from '../core/processor-proxy';
 
 const { log } = getLogger('HlsToMediaSourceFlow', LoggerLevel.ON, true);
 
@@ -31,6 +32,11 @@ export class HlsToMediaSourceFlow extends Flow {
   private _haveVideo = false;
   private _haveAudio = false;
   private _mediaSource: MediaSource;
+
+  private mp4DemuxProc_: ProcessorProxy;
+  private tsDemuxProc_: ProcessorProxy;
+  private h264ParseProc_: ProcessorProxy;
+  private mp4MuxProc_: ProcessorProxy;
 
   constructor (private _m3u8Url: string, private _videoEl: HTMLMediaElement) {
     super(
@@ -48,78 +54,72 @@ export class HlsToMediaSourceFlow extends Flow {
   protected onWaitingToFlowing_ (done: VoidCallback) {
     this._hlsOutSocket.load(this._m3u8Url);
     this._hlsOutSocket.whenReady().then(() => {
-      this._mediaSource.duration = 60;
+      this._mediaSource.duration = 10;
       this._hlsOutSocket.seek(0, 10);
     });
     done();
   }
 
+  private onDemuxOutputCreated_ = (data: ProcessorEventData) => {
+    log('demuxer output created');
+
+    const demuxOutputSocket = <OutputSocket> data.socket;
+    const payloadDescriptor = demuxOutputSocket.payload();
+
+    let mp4MuxerInputSocket;
+
+    if (data.processor === this.mp4DemuxProc_) {
+
+      demuxOutputSocket.connect(this.h264ParseProc_.in[0]);
+      mp4MuxerInputSocket = this.mp4MuxProc_.createInput();
+      this.h264ParseProc_.out[0].connect(mp4MuxerInputSocket);
+
+    } else if (data.processor === this.tsDemuxProc_) {
+
+      if (!this._haveVideo &&
+          PayloadCodec.isAvc(payloadDescriptor.codec)) {
+
+        log('got video payload');
+        this._haveVideo = true;
+
+        demuxOutputSocket.connect(this.h264ParseProc_.in[0]);
+        mp4MuxerInputSocket = this.mp4MuxProc_.createInput();
+        this.h264ParseProc_.out[0].connect(mp4MuxerInputSocket);
+
+      } else if (!this._haveAudio &&
+          PayloadCodec.isAac(payloadDescriptor.codec)) {
+
+        log('got audio payload');
+
+        /*
+        this._haveAudio = true;
+        muxerInputSocket = mp4MuxProc.createInput();
+        demuxOutputSocket.connect(muxerInputSocket);
+        */
+
+      }
+    }
+  };
+
   protected onVoidToWaiting_ (done: VoidCallback) {
 
     const mediaSource = this._mediaSource = new MediaSource();
-    this._videoEl.src = URL.createObjectURL(mediaSource);
-
     const inSocket = this._mseInSocket = new MediaSourceInputSocket(mediaSource, 'video/mp4');
 
-    const mp4DemuxProc = newProcessorWorkerShell(MP4DemuxProcessor);
-    const tsDemuxProc = newProcessorWorkerShell(MPEGTSDemuxProcessor);
-    const h264ParseProc = newProcessorWorkerShell(H264ParseProcessor);
+    const mp4DemuxProc = this.mp4DemuxProc_ = newProcessorWorkerShell(MP4DemuxProcessor);
+    const tsDemuxProc = this.tsDemuxProc_ = newProcessorWorkerShell(MPEGTSDemuxProcessor);
+    const h264ParseProc = this.h264ParseProc_ = newProcessorWorkerShell(H264ParseProcessor);
     const mp4MuxOptions: Partial<MP4MuxProcessorOptions> = {
       fragmentedMode: true
     }
-    const mp4MuxProc = newProcessorWorkerShell(unsafeCastProcessorType(MP4MuxProcessor), [mp4MuxOptions]);
+    const mp4MuxProc = this.mp4MuxProc_ = newProcessorWorkerShell(unsafeCastProcessorType(MP4MuxProcessor), [mp4MuxOptions]);
 
     const outSocket = this._hlsOutSocket = new HlsOutputSocket();
 
-    const onDemuxOutputCreated = (data: ProcessorEventData) => {
-      const demuxOutputSocket = <OutputSocket> data.socket;
-
-      log('demuxer output created');
-
-      let mp4MuxerInputSocket;
-
-      const payloadDescriptor = demuxOutputSocket.payload();
-
-      if (data.processor === mp4DemuxProc) {
-
-        demuxOutputSocket.connect(h264ParseProc.in[0]);
-
-        mp4MuxerInputSocket = mp4MuxProc.createInput();
-        h264ParseProc.out[0].connect(mp4MuxerInputSocket);
-
-      } else if (data.processor === tsDemuxProc) {
-
-        if (!this._haveVideo &&
-            PayloadCodec.isAvc(payloadDescriptor.codec)) {
-
-          log('got video payload');
-
-          this._haveVideo = true;
-
-          demuxOutputSocket.connect(h264ParseProc.in[0]);
-
-          mp4MuxerInputSocket = mp4MuxProc.createInput();
-          h264ParseProc.out[0].connect(mp4MuxerInputSocket);
-
-        } else if (!this._haveAudio &&
-            PayloadCodec.isAac(payloadDescriptor.codec)) {
-
-          log('got audio payload');
-
-          /*
-          this._haveAudio = true;
-          muxerInputSocket = mp4MuxProc.createInput();
-          demuxOutputSocket.connect(muxerInputSocket);
-          */
-
-        }
-      }
-    };
-
     this.addProc(mp4DemuxProc, mp4MuxProc, tsDemuxProc, mp4MuxProc);
 
-    tsDemuxProc.on(ProcessorEvent.OUTPUT_SOCKET_CREATED, onDemuxOutputCreated);
-    mp4DemuxProc.on(ProcessorEvent.OUTPUT_SOCKET_CREATED, onDemuxOutputCreated);
+    tsDemuxProc.on(ProcessorEvent.OUTPUT_SOCKET_CREATED, this.onDemuxOutputCreated_.bind(this));
+    mp4DemuxProc.on(ProcessorEvent.OUTPUT_SOCKET_CREATED, this.onDemuxOutputCreated_.bind(this));
 
     if (USE_TS_DEMUX) { // FIXME use mime-type of response
       outSocket.connect(tsDemuxProc.in[0]);
@@ -129,6 +129,9 @@ export class HlsToMediaSourceFlow extends Flow {
 
     mp4MuxProc.out[0].connect(inSocket);
     //this.connectWithAllExternalSockets(mp4MuxProc.out[0]);
+
+    this._videoEl.src = URL.createObjectURL(mediaSource);
+    this._videoEl.play();
 
     done();
   }
