730648f42ee5de68980f1b079fa0d52501fe7f24
Stephan Hesse <stephan@emliri.com>
2020-05-06 03:32:55 +0200

    export AppInputSocket to public lib index

--- src/io-sockets/index.ts
+++ src/io-sockets/index.ts
@@ -10,3 +10,6 @@ export { NodeFsReadSocket } from './src/io-sockets/node-fs-read.socket';
 export { WebFileChooserSocket } from './web-file-chooser.socket';
 export { MediaSourceInputSocket } from './mse-input.socket';
 export { WebFileDownloadSocket } from './web-file-download.socket';
+
+export {AppInputSocket} from './app-input-socket';
+export {AppOutputSocket, AppOutputSocketAsyncFunc, AppOutputSocketSyncFunc} from './app-output-socket';
