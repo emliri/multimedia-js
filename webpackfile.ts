dd854ff8c47b73dc33e7e9429fbfb45f96de5656
Stephan Hesse <stephan@emliri.com>
2020-05-12 01:18:22 +0200

    add NO_FRILLS=1 build env var to only compile essential dist asset

--- webpackfile.ts
+++ webpackfile.ts
@@ -2,6 +2,7 @@ import {createWebpackConfig} from './webpack-config-factory'
 
 const configs = []
 const debug = process.env.DEBUG === '1'
+const noFrills = process.env.NO_FRILLS === '1'
 
 const exec = require('child_process').exec;
 
@@ -37,123 +38,127 @@ const afterEmit = { // custom "AfterEmitPlugin" to exec shell script post-build
   )
 }
 
-// Processor-proxy worker
-{
-  const entrySrc = './src/core/processor-proxy.worker.ts'
-  const libName = 'mmjs-procs-worker'
-  const buildPath = 'dist'
-  const libraryTarget = 'umd'
+if (!noFrills) {
 
-  configs.push(
-    createWebpackConfig({
-      debug,
-      entrySrc,
-      libName,
-      libraryTarget,
-      buildPath
-    })
-  )
+  // Processor-proxy worker
+  {
+    const entrySrc = './src/core/processor-proxy.worker.ts'
+    const libName = 'mmjs-procs-worker'
+    const buildPath = 'dist'
+    const libraryTarget = 'umd'
+
+    configs.push(
+      createWebpackConfig({
+        debug,
+        entrySrc,
+        libName,
+        libraryTarget,
+        buildPath
+      })
+    )
+  }
+
+  // TestCasesWeb
+  {
+    const entrySrc = './test-cases/web/index.ts'
+    const libName = 'mmjs-test-cases'
+    const buildPath = 'dist'
+    const libraryTarget = 'umd'
+
+    configs.push(
+      createWebpackConfig({
+        debug,
+        entrySrc,
+        libName,
+        libraryTarget,
+        buildPath
+      })
+    )
+  }
+
+  /*
+
+  // Procs Library
+  {
+    const entrySrc = './src/processors/index.ts'
+    const libName = 'mmjs-procs'
+    const buildPath = 'dist'
+    const libraryTarget = 'umd'
+    const debug = true
+
+    configs.push(
+      createWebpackConfig({
+        debug,
+        entrySrc,
+        libName,
+        libraryTarget,
+        buildPath
+      })
+    )
+  }
+
+  // Flows Library
+  {
+    const entrySrc = './src/flows/index.ts'
+    const libName = 'mmjs-flows'
+    const buildPath = 'dist'
+    const libraryTarget = 'umd'
+    const debug = true
+
+    configs.push(
+      createWebpackConfig({
+        debug,
+        entrySrc,
+        libName,
+        libraryTarget,
+        buildPath
+      })
+    )
+  }
+
+  // I/O-Sockets Library
+  {
+    const entrySrc = './src/io-sockets/index.ts'
+    const libName = 'mmjs-io-sockets'
+    const buildPath = 'dist'
+    const libraryTarget = 'umd'
+    const debug = true
+
+    configs.push(
+      createWebpackConfig({
+        debug,
+        entrySrc,
+        libName,
+        libraryTarget,
+        buildPath
+      })
+    )
+  }
+  */
+
+
+  // Task worker
+  /*
+  {
+    const entrySrc = './src/core/processor-task.worker.ts'
+    const libName = 'MMProcessorTaskWorker'
+    const buildPath = 'dist'
+    const libraryTarget = 'umd'
+    const debug = true
+
+    configs.push(
+      createWebpackConfig({
+        debug,
+        entrySrc,
+        libName,
+        libraryTarget,
+        buildPath
+      })
+    )
+  }
+  */
 }
 
-// TestCasesWeb
-{
-  const entrySrc = './test-cases/web/index.ts'
-  const libName = 'mmjs-test-cases'
-  const buildPath = 'dist'
-  const libraryTarget = 'umd'
-
-  configs.push(
-    createWebpackConfig({
-      debug,
-      entrySrc,
-      libName,
-      libraryTarget,
-      buildPath
-    })
-  )
-}
-
-/*
-
-// Procs Library
-{
-  const entrySrc = './src/processors/index.ts'
-  const libName = 'mmjs-procs'
-  const buildPath = 'dist'
-  const libraryTarget = 'umd'
-  const debug = true
-
-  configs.push(
-    createWebpackConfig({
-      debug,
-      entrySrc,
-      libName,
-      libraryTarget,
-      buildPath
-    })
-  )
-}
-
-// Flows Library
-{
-  const entrySrc = './src/flows/index.ts'
-  const libName = 'mmjs-flows'
-  const buildPath = 'dist'
-  const libraryTarget = 'umd'
-  const debug = true
-
-  configs.push(
-    createWebpackConfig({
-      debug,
-      entrySrc,
-      libName,
-      libraryTarget,
-      buildPath
-    })
-  )
-}
-
-// I/O-Sockets Library
-{
-  const entrySrc = './src/io-sockets/index.ts'
-  const libName = 'mmjs-io-sockets'
-  const buildPath = 'dist'
-  const libraryTarget = 'umd'
-  const debug = true
-
-  configs.push(
-    createWebpackConfig({
-      debug,
-      entrySrc,
-      libName,
-      libraryTarget,
-      buildPath
-    })
-  )
-}
-*/
-
-
-// Task worker
-/*
-{
-  const entrySrc = './src/core/processor-task.worker.ts'
-  const libName = 'MMProcessorTaskWorker'
-  const buildPath = 'dist'
-  const libraryTarget = 'umd'
-  const debug = true
-
-  configs.push(
-    createWebpackConfig({
-      debug,
-      entrySrc,
-      libName,
-      libraryTarget,
-      buildPath
-    })
-  )
-}
-*/
 
 
 export default configs
