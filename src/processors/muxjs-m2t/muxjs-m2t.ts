a208cf0d23b5abb82ac063513ead761cca81f37d
Stephan Hesse <stephan@emliri.com>
2021-05-13 13:39:00 +0200

    upgrade typescript + finally fix many error on declaration-only emit job (isolated config for build-decls, some tricks + upgrade webpack&node types)

--- src/processors/muxjs-m2t/muxjs-m2t.ts
+++ src/processors/muxjs-m2t/muxjs-m2t.ts
@@ -1,3 +1,9 @@
+import * as AdtsStream from '../../ext-mod/mux.js/lib/codecs/adts.js';
+import * as H264Codec from '../../ext-mod/mux.js/lib/codecs/h264'
+
+export {AdtsStream};
+export {H264Codec};
+
 export {
   ElementaryStream,
   TransportPacketStream,
@@ -7,5 +13,4 @@ export {
   MetadataStream
 } from '../../ext-mod/mux.js/lib/m2ts/m2ts';
 
-export * as AdtsStream from '../../ext-mod/mux.js/lib/codecs/adts.js';
-export * as H264Codec from '../../ext-mod/mux.js/lib/codecs/h264'
+
