18233970b156f69a8b1070b8f749d0aed679f287
Stephan Hesse <stephan@emliri.com>
2021-06-27 02:15:55 +0200

    socket-tap-tokens: implement with bucket & rate counter

--- src/socket-taps/socktap-tokens.ts
+++ src/socket-taps/socktap-tokens.ts
@@ -1,12 +1,61 @@
+import { Nullable } from '../common-types';
 import { Packet } from '../core/packet';
 import { SocketTapDefault } from '../core/socket-tap';
 import { TokenBucketPacketQueue } from '../lib/token-bucket';
+import { TokenRate } from '../lib/token-rate';
 
-export class SocketTapTokenRate extends SocketTapDefault {
-  private _tokenBucket = new TokenBucketPacketQueue<Packet>();
+export class SocketTapTokenBucket extends SocketTapDefault {
+  private _tokenBucket: TokenBucketPacketQueue<Packet>;
+  private _tokenRateIn = new TokenRate(this._rateDeltaMs);
+  private _tokenRateOut = new TokenRate(this._rateDeltaMs);
+  private _packetQ: Packet[] = [];
+
+  constructor(private _rateDeltaMs: number = 1000) {
+    super();
+    this._tokenBucket = new TokenBucketPacketQueue((p_, p) => {
+      this._onPop(p);
+    });
+  }
+
+  set byteRateLimit(rate: number) {
+    this._tokenBucket.tokenRate = rate;
+  }
+
+  get byteRateIn() {
+    return this._tokenRateIn.value();
+  }
+
+  get byteRateOut() {
+    return this._tokenRateIn.value();
+  }
 
   pushPacket (p: Packet): boolean {
+    this._tokenRateIn.add(p.byteLength);
     this._tokenBucket.pushPacket(p, p);
-    return true;
+
+    return false;
   }
+
+  popPacket (): Nullable<Packet> {
+    const p = this._packetQ.shift() || null;
+    if (p) {
+      this._tokenRateOut.add(p.byteLength);
+    }
+    return p;
+  }
+
+  isClear() {
+    return this._packetQ.length === 0;
+  }
+
+  flush () {
+    this._packetQ.length = 0;
+    this._tokenBucket.tokenRate = Infinity;
+    this._tokenBucket.reset();
+  }
+
+  private _onPop(p: Packet) {
+    this._packetQ.push(p);
+  }
+
 }
