c8c9df52f4cdba3c27938045cf9b9389931423fb
Stephan Hesse <stephan@emliri.com>
2021-06-27 03:42:39 +0200

    add socket ref prop to createAsyncTransferFunc return val

--- src/io-sockets/app-output-socket.ts
+++ src/io-sockets/app-output-socket.ts
@@ -5,9 +5,13 @@ export type AppOutputSocketAsyncFunc = (p: Packet) => Promise<boolean>;
 export type AppOutputSocketSyncFunc = (p: Packet) => boolean;
 
 export class AppOutputSocket extends OutputSocket {
-  static createAsyncTransferFunc (descriptor: SocketDescriptor, inputSocket?: InputSocket): (p: Packet) => Promise<boolean> {
-    let socket;
-    return (socket = new AppOutputSocket(descriptor, inputSocket)).transfer.bind(socket);
+  static createAsyncTransferFunc (descriptor: SocketDescriptor, inputSocket?: InputSocket): {func: (p: Packet) => Promise<boolean>, sock: OutputSocket} {
+    const socket: AppOutputSocket = new AppOutputSocket(descriptor, inputSocket);
+    const func = socket.transfer.bind(socket);
+    return {
+      func,
+      sock: socket
+    }
   }
 
   /*
