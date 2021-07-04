29e6ed9c6c83a64e28dfc32d79b17fcad71a6796
Stephan Hesse <stephan@emliri.com>
2021-07-05 01:40:13 +0200

    add Mp4CmafNetStreamParseProc export to proc index

--- src/processors/index.ts
+++ src/processors/index.ts
@@ -3,6 +3,7 @@ export { AVCNetworkAbstractionProcessor } from './avc-network-abstraction.proc';
 export { FFmpegConvertProcessor } from './ffmpeg-convert.processor';
 export { MP4MuxProcessor } from './mp4-mux-mozilla.processor';
 export { MP4DemuxProcessor } from './mp4-demux.processor';
+export { Mp4CmafNetStreamParseProc } from './mp4cmaf-parse.proc';
 export { MP2TSDemuxProcessor } from './mp2ts-demux.proc';
 
 export { LambdaProcessor } from './lambda.processor';
