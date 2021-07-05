cab9e72a2f29b0be3e8a4b8415107f35f67510fb
Stephan Hesse <stephan@emliri.com>
2021-07-05 03:33:51 +0200

    socktap pcap: add debug log dump option

--- src/socket-taps/socktap-packet-capture.ts
+++ src/socket-taps/socktap-packet-capture.ts
@@ -1,12 +1,21 @@
 import { SocketTapDefault } from '../core/socket-tap';
 import { PacketDataModel } from '../core/packet-model';
 import { Packet } from '../core/packet';
+import { arrayLast, prntprtty } from '../common-utils';
 
 export class SocketTapPacketCapture extends SocketTapDefault {
+
+  constructor(private _debugLog: boolean = false) {
+    super();
+  }
+
   readonly dataList: PacketDataModel[] = [];
 
   pushPacket (p: Packet): boolean {
     this.dataList.push(PacketDataModel.createFromPacket(p));
+    if (this._debugLog) {
+      console.debug(prntprtty(arrayLast(this.dataList)));
+    }
     return true;
   }
 
