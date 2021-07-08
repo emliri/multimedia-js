9b9018df314a2e6e8751f57cb4c2b0ca310b964a
Stephan Hesse <stephan@emliri.com>
2021-07-08 19:24:32 +0200

    mpts-analyzer spec: fix test-data path

--- src/processors/mp2ts-analyzer.proc.spec.ts
+++ src/processors/mp2ts-analyzer.proc.spec.ts
@@ -11,7 +11,7 @@ import { Mp2TsAnalyzerProc } from './mp2ts-analyzer.proc';
 describeSpecTopLevel(__filename, () => {
   it('should output a model packet for each PES complete A/V sample/frame', async (done) => {
     const tsData = await readFile(path.resolve(__dirname,
-      '../../test-data/smptebars-beeps-mainhd30fps-15-secs.ts'));
+      '../../test-data/mp2ts/smptebars-beeps-mainhd30fps-15-secs.ts'));
 
     const proc = new Mp2TsAnalyzerProc();
 
