ce5aa61d343e799660191347ab2fec5b86899d89
Stephan Hesse <stephan@emliri.com>
2021-08-04 06:41:45 +0200

    Mp2TsAnalyzerProc added to proc lib index

--- src/processors/index.ts
+++ src/processors/index.ts
@@ -5,5 +5,6 @@ export { MP4MuxProcessor } from './mp4-mux-mozilla.processor';
 export { MP4DemuxProcessor } from './mp4-demux.processor';
 export { Mp4CmafNetStreamParseProc } from './mp4cmaf-parse.proc';
 export { MP2TSDemuxProcessor } from './mp2ts-demux.proc';
+export { Mp2TsAnalyzerProc } from './mp2ts-analyzer.proc';
 
 export { LambdaProcessor } from './lambda.processor';
