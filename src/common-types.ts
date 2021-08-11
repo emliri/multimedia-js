9c446cd1f4b8cef6bcf80cdef1a6245773b8e7ff
Stephan Hesse <stephan@emliri.com>
2021-08-11 17:34:15 +0200

    common types: add generic constructor/class helpers & mixin type union

--- src/common-types.ts
+++ src/common-types.ts
@@ -8,6 +8,14 @@ export type LambdaNoArgFunc = () => void;
 
 export type TwoDimArray<T> = T[][];
 
-export type Constructor = new (...args: any[]) => {};
+// Constructor-types as per the TypeScript Handbook
+export type ObjectConstructor = new (...args: any[]) => {};
+export type GenericConstructor<T = {}> = new (...args: any[]) => T;
+// Generic constructor-type notation in an own twist
+export type ConstructorOf<T extends Object> = new (...args: any[]) => T;
+
+export type Class<T> = ConstructorOf<T>
+
+export type Mixin<T1, T2> = ConstructorOf<T1> & ConstructorOf<T2>
 
 
