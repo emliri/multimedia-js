33f607895d13fde1e23746c5f232c371b67090e1
Stephan Hesse <stephan@emliri.com>
2021-11-03 20:09:24 +0100

    mpts utils: add max timestamp values & mp4-wrapover value

--- src/processors/mpeg2ts/mpeg2ts-utils.ts
+++ src/processors/mpeg2ts/mpeg2ts-utils.ts
@@ -3,6 +3,9 @@ export const MPEG2TS_PACKET_SIZE: number = 188 as const;
 export const MPEG2TS_SYNC_CHAR = 0x47 as const;
 export const MPEG2TS_MAX_SCAN_WINDOW = 4096 as const;
 export const MPEG_TS_TIMESCALE_HZ = 90000 as const;
+export const MPEG_TS_MAX_TIMESTAMP = (Math.pow(2, 33) - 1);
+export const MPEG_TS_MAX_TIMESTAMP_SECS = MPEG_TS_MAX_TIMESTAMP / MPEG_TS_TIMESCALE_HZ;
+export const MP4_CMAF_MAX_TIME_SECS = MPEG_TS_MAX_TIMESTAMP_SECS / 2; // baseMediaDecodeTime uses 32 bits
 
 export function findSyncOffsetInMpegTsChunk (data: Uint8Array): number | null {
   // scan 4096 first bytes
