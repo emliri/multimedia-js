3a1645ad796830cd429be64f8d54a71315151d6d
Stephan Hesse <stephan@emliri.com>
2021-12-02 03:07:05 +0100

    aac-js wrapper: hardcode "cookie" data from assumed ADTS header infos + general event harness setup

--- src/processors/aac/aac-js-dec.ts
+++ src/processors/aac/aac-js-dec.ts
@@ -2,13 +2,12 @@
 import { EventEmitter } from "eventemitter3";
 
 const AacDecoder = require('../../ext-mod/aac.js/src/decoder');
+const Aurora = require('../../ext-mod/aurora.js/build/aurora');
 
-var Aurora = require('../../ext-mod/aurora.js/build/aurora');
-
-class AacJsAuroraDemuxer extends EventEmitter {
+class AacJsAuroraDemuxerShim extends EventEmitter {
 
   pushBuffer(data: Uint8Array) {
-    const avBuffer = new Aurora.AVBuffer(data);
+    const avBuffer = new Aurora.Buffer(data);
     this.emit('data', avBuffer);
   }
 
@@ -19,10 +18,57 @@ class AacJsAuroraDemuxer extends EventEmitter {
 
 export class AacJsDecoder {
 
-  private _aacDemux: AacJsAuroraDemuxer = new AacJsAuroraDemuxer();
+  private _aacDemux: AacJsAuroraDemuxerShim = new AacJsAuroraDemuxerShim();
   private _aacDec = new AacDecoder(this._aacDemux, {});
 
   constructor() {
+    this._aacDec.on('data', (data) => {
+      this.onData(data);
+    });
+    this._aacDec.on('end', () => {
+      this.onEos();
+    });
+    this._aacDec.on('error', (err) => {
+      this.onError(err);
+    });
 
+    // generate a magic cookie from the ADTS header
+    const header = {
+      profile: 2,
+      samplingIndex: 4,
+      chanConfig: 2
+    }
+    var cookie = new Uint8Array(2);
+    cookie[0] = (header.profile << 3) | ((header.samplingIndex >> 1) & 7);
+    cookie[1] = ((header.samplingIndex & 1) << 7) | (header.chanConfig << 3);
+    this._aacDec.setCookie(new Aurora.Buffer(cookie));
+  }
+
+  dispose() {
+    this._aacDemux.eos();
+    this._aacDemux.removeAllListeners();
+    this._aacDemux = null;
+    this._aacDec = null;
+  }
+
+  pushBuffer(buf: Uint8Array) {
+    if (!this._aacDemux) {
+      throw new Error('Decoder already disposed, cant push data');
+    }
+    this._aacDemux.pushBuffer(buf);
+    this._aacDec.decode();
+
+  }
+
+  onData(data) {
+    console.log(data);
+  }
+
+  onEos() {
+    console.log('AAC.js "end" event (EOS)');
+  }
+
+  onError(err) {
+    console.error(err);
   }
 }
