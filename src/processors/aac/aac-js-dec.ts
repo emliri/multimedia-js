6db894f7bd3d42d61bdd3d74864bb02214670a75
Stephan Hesse <stephan@emliri.com>
2022-02-19 19:20:31 +0100

    aac-js-dec: some default-opts details + autofix lint

--- src/processors/aac/aac-js-dec.ts
+++ src/processors/aac/aac-js-dec.ts
@@ -1,22 +1,21 @@
 
-import { EventEmitter } from "eventemitter3";
+import { EventEmitter } from 'eventemitter3';
 
-import { Nullable } from "../../common-types";
-import { isTypedArraySharingBuffer } from "../../common-utils";
+import { Nullable } from '../../common-types';
+import { isTypedArraySharingBuffer } from '../../common-utils';
 
-import { splitRawAudioFrameToStereoChannels } from "./aac-utils";
+import { splitRawAudioFrameToStereoChannels } from './aac-utils';
 
 const AacDecoder = require('../../ext-mod/aac.js/src/decoder');
 const AuroraAv = require('../../ext-mod/aac.js/src/av');
 
 class AacJsAuroraDemuxerShim extends EventEmitter {
-
-  pushBuffer(data: Uint8Array) {
+  pushBuffer (data: Uint8Array) {
     const avBuffer = new AuroraAv.Buffer(data);
     this.emit('data', avBuffer);
   }
 
-  eos() {
+  eos () {
     this.emit('end');
   }
 }
@@ -27,21 +26,21 @@ export type AacJsDecoderConfig = {
   channels: number
 }
 
-const defaultConfig: AacJsDecoderConfig = {
-  profile: 2,
-  samplingIndex: 4,
-  channels: 2
-}
-
 export class AacJsDecoder {
+  static get defaultConfig (): AacJsDecoderConfig {
+    return {
+      profile: 2,
+      samplingIndex: 4,
+      channels: 2
+    };
+  }
 
   private _aacDemux: AacJsAuroraDemuxerShim = new AacJsAuroraDemuxerShim();
   private _aacDec = new AacDecoder(this._aacDemux, {});
 
-  constructor(header: Nullable<AacJsDecoderConfig>, enableStereoChannelSplit: boolean = false) {
-
+  constructor (header: Nullable<AacJsDecoderConfig>, enableStereoChannelSplit: boolean = false) {
     if (!header) {
-      header = defaultConfig;
+      header = AacJsDecoder.defaultConfig;
     }
 
 
@@ -69,14 +68,14 @@ export class AacJsDecoder {
     this._aacDec.setCookie(new AuroraAv.Buffer(cookie));
   }
 
-  dispose() {
+  dispose () {
     this._aacDemux.eos();
     this._aacDemux.removeAllListeners();
     this._aacDemux = null;
     this._aacDec = null;
   }
 
-  pushBuffer(buf: Uint8Array) {
+  pushBuffer (buf: Uint8Array) {
     if (!this._aacDemux) {
       throw new Error('Decoder already disposed, cant push data');
     }
@@ -84,25 +83,24 @@ export class AacJsDecoder {
     this._aacDec.decode();
   }
 
-  onData(data: Float32Array[]) {
+  onData (data: Float32Array[]) {
     console.log('AAC.js-decoder output:', data);
   }
 
-  onEos() {
+  onEos () {
     console.log('AAC.js-decoder EOS event');
   }
 
-  onError(err: Error) {
+  onError (err: Error) {
     console.error('AAC.js-decoder error event:', err);
   }
 }
 
 export class AacJsDecoderWorkerContext {
-
   private _workerCtx = self as any;
 
-  constructor(cfg: AacJsDecoderConfig = defaultConfig, enableStereoChannelSplit: boolean = false) {
-
+  constructor (cfg: AacJsDecoderConfig = AacJsDecoder.defaultConfig,
+    enableStereoChannelSplit: boolean = false) {
     if (!this._workerCtx.importScripts) {
       throw new Error('Class should only be constructed from Worker scope');
     }
@@ -112,11 +110,10 @@ export class AacJsDecoderWorkerContext {
     const workerCtx: Worker = this._workerCtx as Worker;
     self.addEventListener('message', (event) => {
       const buf: Uint8Array = event.data;
-      dec.pushBuffer(buf)
+      dec.pushBuffer(buf);
     });
 
     dec.onData = (audioFrame: Float32Array[]) => {
-
       const isSharingBuffer = audioFrame.some((buf) => isTypedArraySharingBuffer(buf));
       if (isSharingBuffer) {
         throw new Error('Cant transfer audio-frame, is not unique owner of its ArrayBuffer memory');
@@ -128,10 +125,10 @@ export class AacJsDecoderWorkerContext {
 
     dec.onError = (err: Error) => {
       console.error(err);
-    }
+    };
 
     dec.onEos = () => {
       console.warn('AAC-js worker: EOS');
-    }
+    };
   }
 }
