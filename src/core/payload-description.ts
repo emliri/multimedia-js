9175e3e13c329453115d6405909b666058e67e30
Stephan Hesse <stephan@emliri.com>
2022-03-03 03:32:20 +0100

    core: add VIDEO_H264_SEI in common mimetypes

--- src/core/payload-description.ts
+++ src/core/payload-description.ts
@@ -26,6 +26,7 @@ export enum CommonMimeTypes {
   VIDEO_MP4 = 'video/mp4',
   VIDEO_AVC = 'video/avc', // access units
   VIDEO_H264 = 'video/h264', // raw NALUs
+  VIDEO_H264_SEI = 'video/h264-sei', // only SEI data
   VIDEO_MPEGTS = 'video/MP2T',
   APP_HLS = 'application/vnd.apple.mpegurl',
   APP_XMPEGURL = 'application/x-mpegurl'
